cabe9cc72d11fd2a998ff05dec026c5466d7695d
Hsiao Yao <tomtogo1260@gmail.com>
2024-02-17 04:07:48 +0800

    calculate 1st level bounding info

--- src/core/build/buildTree.js
+++ src/core/build/buildTree.js
@@ -1,5 +1,24 @@
 import { ensureIndex, getTriCount } from "./geometryUtils.js";
-import { computeTriangleBounds } from "./computeBoundsUtils.js";
+import { getBounds, computeTriangleBounds } from "./computeBoundsUtils.js";
+import { MeshBVHNode } from "../MeshBVHNode.js";
+
+export function buildTree(bvh, triangleBounds, offset, count, options) {
+	// epxand variables
+	const { maxLeafTris } = options;
+
+	const cacheCentroidBoundingData = new Float32Array(6);
+
+	const root = new MeshBVHNode();
+	getBounds(
+		triangleBounds,
+		offset,
+		count,
+		root.boundingData,
+		cacheCentroidBoundingData
+	);
+	console.log(root.boundingData, cacheCentroidBoundingData);
+	return root;
+}
 
 export function buildPackedTree(bvh, options) {
 	const geometry = bvh.geometry;
@@ -10,5 +29,16 @@ export function buildPackedTree(bvh, options) {
 	const triangleBounds = computeTriangleBounds(geometry);
 	const geometryRanges = [{ offset: 0, count: getTriCount(geometry) }];
 
-	console.log(triangleBounds, geometryRanges);
+	bvh._roots = geometryRanges.map((range) => {
+		const root = buildTree(
+			bvh,
+			triangleBounds,
+			range.offset,
+			range.count,
+			options
+		);
+		console.log(root);
+
+		return null;
+	});
 }
--- src/core/build/computeBoundsUtils.js
+++ src/core/build/computeBoundsUtils.js
@@ -1,6 +1,76 @@
 import { FLOAT32_EPSILON } from "../Constants.js";
 import { getTriCount } from "./geometryUtils.js";
 
+// computes the union of the bounds of all of the given triangles and puts the resulting box in "target".
+// A bounding box is computed for the centroids of the triangles, as well, and placed in "centroidTarget".
+// These are computed together to avoid redundant accesses to bounds array.
+export function getBounds(
+	triangleBounds,
+	offset,
+	count,
+	target,
+	centroidTarget
+) {
+	let minx = Infinity;
+	let miny = Infinity;
+	let minz = Infinity;
+	let maxx = -Infinity;
+	let maxy = -Infinity;
+	let maxz = -Infinity;
+
+	let cminx = Infinity;
+	let cminy = Infinity;
+	let cminz = Infinity;
+	let cmaxx = -Infinity;
+	let cmaxy = -Infinity;
+	let cmaxz = -Infinity;
+
+	for (let i = offset * 6, end = (offset + count) * 6; i < end; i += 6) {
+		const cx = triangleBounds[i + 0];
+		const hx = triangleBounds[i + 1];
+		const lx = cx - hx;
+		const rx = cx + hx;
+		if (lx < minx) minx = lx;
+		if (rx > maxx) maxx = rx;
+		if (cx < cminx) cminx = cx;
+		if (cx > cmaxx) cmaxx = cx;
+
+		const cy = triangleBounds[i + 2];
+		const hy = triangleBounds[i + 3];
+		const ly = cy - hy;
+		const ry = cy + hy;
+		if (ly < miny) miny = ly;
+		if (ry > maxy) maxy = ry;
+		if (cy < cminy) cminy = cy;
+		if (cy > cmaxy) cmaxy = cy;
+
+		const cz = triangleBounds[i + 4];
+		const hz = triangleBounds[i + 5];
+		const lz = cz - hz;
+		const rz = cz + hz;
+		if (lz < minz) minz = lz;
+		if (rz > maxz) maxz = rz;
+		if (cz < cminz) cminz = cz;
+		if (cz > cmaxz) cmaxz = cz;
+	}
+
+	target[0] = minx;
+	target[1] = miny;
+	target[2] = minz;
+
+	target[3] = maxx;
+	target[4] = maxy;
+	target[5] = maxz;
+
+	centroidTarget[0] = cminx;
+	centroidTarget[1] = cminy;
+	centroidTarget[2] = cminz;
+
+	centroidTarget[3] = cmaxx;
+	centroidTarget[4] = cmaxy;
+	centroidTarget[5] = cmaxz;
+}
+
 // precomputes the bounding box for each triangle; required for quickly calculating tree splits.
 // result is an array of size tris.length * 6 where triangle i maps to a
 // [x_center, x_delta, y_center, y_delta, z_center, z_delta] tuple starting at index i * 6,
